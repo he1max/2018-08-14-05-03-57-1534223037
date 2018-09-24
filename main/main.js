0a7e88883ab1ca2d476374cceecc3c8b1e908e53
he1max <he1max@163.com>
2018-09-24 09:37:11 +0800

    require the string's sum

--- main/main.js
+++ main/main.js
@@ -1,3 +1,7 @@
-module.exports = function main() {
-  // Write your code here
+module.exports = function main(a) {
+	let sum=0;
+	for(let i=0;i<a.length;i++){
+		sum+=parseInt(a[i]);                    //字符串求和
+	}
+	return sum;
 };
